d6071bf290d87abc1d7c12fd6b90809d123eaba3
Evan Stern <evanstern@machineservant.com>
2023-06-03 08:37:28 -0400

    Get tsc -p cypress to run

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: 'kk33ag',
   e2e: {
     setupNodeEvents: (on, config) => {
       const isDev = config.watchForFileChanges;
